a8c60bf294b522f855032c80b69428c628a21b29
r2d2grl <r2d2krl@gmail.com>
2024-02-04 12:05:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -71,6 +71,7 @@ function displayForecast() {
       `
       <div class="weather-forecast">
       <div class="row">
+      <div class="col-2">
       <div class="weather-forecast-date">${day}</div>
               <img
                 src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png"
@@ -81,6 +82,7 @@ function displayForecast() {
                 <span class="weather-forecast-temperature-max">18°</span>
                 <span class="weather-forecast-temperature-min">12°</span>
               </div>
+      </div>
       </div>
       </div>
       `;
