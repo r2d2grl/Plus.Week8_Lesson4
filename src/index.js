f5091002d74493e070993faad3af3ac209b656b1
r2d2grl <r2d2krl@gmail.com>
2024-02-04 12:01:28 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -71,8 +71,7 @@ function displayForecast() {
       `
       <div class="weather-forecast">
       <div class="row">
-            <div class="col-2">
-              <div class="weather-forecast-date">${day}</div>
+      <div class="weather-forecast-date">${day}</div>
               <img
                 src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png"
                 alt=""
@@ -82,7 +81,7 @@ function displayForecast() {
                 <span class="weather-forecast-temperature-max">18°</span>
                 <span class="weather-forecast-temperature-min">12°</span>
               </div>
-            </div>
+      </div>
       </div>
       `;
   });
